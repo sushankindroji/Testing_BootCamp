67852f5eb546cf3c642dc2bc31d8bdc24b86cb48
Sushank <sushankindroji9@gmail.com>
2026-09-09 14:27:59 +0530

    Add Selenium Multiple Frames Maven project with Jenkinsfile

---
+++ Automation/SeleniumWebDriverMultipleFrames/src/test/java/multipleFrames/MultipleFrames.java
@@ -0,0 +1,66 @@
+package multipleFrames;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import utility.Helper;
+
+public class MultipleFrames {
+
+	@Test
+	public void Frames() throws Exception {
+		WebDriver driver = Helper.startBrowser("GC");
+		driver.get("file:///Users/sushank2/Documents/Testing/iframes/iframe.html");
+		Thread.sleep(10000);
+	
+		int totalnoofframes = driver.findElements(By.tagName("iframe")).size();
+		System.out.println("Number of Frames: " + totalnoofframes);
+
+		driver.switchTo().frame("easycalculation");
+
+		driver.get("https://www.login.hiox.com/login?referrer=easycalculation.com");
+
+		driver.findElement(By.id("log_email")).sendKeys("8019962379");
+		driver.findElement(By.id("log_password")).sendKeys("Sushank");
+
+		driver.findElement(By.xpath("//input[contains(@name,'log_submit')]")).click();
+
+		String title = driver.getTitle();
+		System.out.println(title);
+	
+		driver.navigate().to("file:///Users/sushank2/Documents/Testing/iframes/iframe.html");
+	
+		Thread.sleep(5000);
+	
+		driver.findElement(By.xpath("/html/body/a")).click();
+	
+		driver.navigate().to("https://www.selenium.dev/");
+	
+		driver.findElement(By.xpath("//*[@id=\"main_navbar\"]/ul/li[3]/a/span")).click();
+	
+		Thread.sleep(5000);
+	
+		String title1 = driver.getTitle();
+		System.out.println(title1);
+	
+		driver.navigate().to("file:///Users/sushank2/Documents/Testing/iframes/iframe.html");
+	
+		driver.switchTo().frame("My Store");
+	
+		driver.navigate().to("https://practicetestautomation.com/practice-test-login/");
+	
+		driver.findElement(By.id("username")).sendKeys("student"); 
+		driver.findElement(By.id("password")).sendKeys("Password123");
+	
+		driver.findElement(By.xpath("//button[@id='submit']")).click();
+	
+		String title2 = driver.getTitle();
+		System.out.println(title2);
+	
+		Thread.sleep(5000);
+	
+		driver.quit();
+
+	}
+}
